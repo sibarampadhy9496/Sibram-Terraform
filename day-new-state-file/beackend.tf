3e04d7a0a14cff56be218b8416dba34ac4ca27df
sibarampadhy9496 <sibarmpadhy9496@gmail.com>
2025-02-24 20:18:33 +0530

    new updated file

--- day-new-state-file/beackend.tf
+++ day-new-state-file/beackend.tf
@@ -1,8 +1,9 @@
  terraform {
 backend "s3" {
-    bucket         = "sib-new"  # Name of the S3 bucket where the state will be stored.
+    bucket         = "sib-new-name" # Name of the S3 bucket where the state will be stored.
     region         =  "ap-south-1"
     key            = "terraform.tfstate" # Path within the bucket where the state will be read/written.
-    
+    dynamodb_table = "terraform-state-lock-dynamo-db" # DynamoDB table used for state locking, note: first run day-4-s3-dynamodb resource then day-4-state-remote-backend
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
 }
 }
